ccc2fdf369340f9c459bb4e3fd697108ffe529af
Jaume Artigues <jaumeartiguesgonzalez1994@gmail.com>
2022-03-04 02:01:01 +0100

    Afegeixo el nou archiu main_LoteriaAlumnes.java al repositori

--- main_LoteriaAlumnes.java
+++ main_LoteriaAlumnes.java
@@ -1,30 +1,19 @@
-import java.util.Scanner;
-
 public class main_LoteriaAlumnes {
 
 	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-
-		int numAlumnes, guanyador;
+	{	
+		//CREO OBJECTE loteria1 DE LA CLASSE classe_LoteriaAlumnes
+		classe_LoteriaAlumnes loteria1 = new classe_LoteriaAlumnes();
 		
-		System.out.println("INTRODUEIX EL NOMBRE D'ALUMNES QUE ENTRARAN A LA LOTERIA:");
-		numAlumnes = sc.nextInt();
+		//COLOCO ELS ALUMNES EN UN ARRAY
+		String alumnes[] = loteria1.colocarAlumnes();
 		
-		String alumnes[] = new String[numAlumnes];
+		//MOSTRO ELS ALUMNES QUE PARTICIPEN A LA LOTERIA
+		loteria1.mostrarAlumnes(alumnes);
 		
-		System.out.println("INTRODUEIX EL NOM DELS ALUMNES QUE ENTRARAN A LA LOTERIA:");
-		for (int i=0; i<alumnes.length; i++)
-			alumnes[i] = sc.next();
-		
-		System.out.println("INTRODUEIX EL NUMERO GUANYADOR DE LA LOTERIA:");
-		guanyador = sc.nextInt();
-		
-		System.out.println("ELS ALUMNES QUE HAN SOBREVISCUT SON: ");
-		for (int i=0; i<alumnes.length; i++)
-			if(alumnes[i]!=alumnes[guanyador])
-				System.out.print(alumnes[i] + " ");
-			
+		//EXECUTO LA LOTERIA
+		loteria1.començaLoteria(alumnes);
+				
 	}
 
 }
